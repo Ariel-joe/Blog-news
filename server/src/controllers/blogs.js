a21ede75dee95cd34b3be7c3ffa326143f132939
Ariel-Joe <arieljoe00@gmail.com>
2025-01-08 22:14:29 +0300

    patch and delete methods

--- server/src/controllers/blogs.js
+++ server/src/controllers/blogs.js
@@ -1,5 +1,43 @@
 import { Blog } from "../database/models/blog.js";
 
+// adding a blog
+export const addBlog = async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      message: newBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    res.Status(500).json({
+      success: false,
+      message: "Failed",
+    });
+  }
+};
+
+// finding a single blog
+export const singleBlog = async (req, res) => {
+  try {
+    const blogId = req.query.id;
+
+    const article = await Blog.findOne({ _id: blogId }, req.body);
+
+    res.json({
+      succcess: true,
+      data: article,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed",
+    });
+  }
+};
+
 // getting all blogs
 export const getBlog = async (req, res) => {
   try {
@@ -18,20 +56,43 @@ export const getBlog = async (req, res) => {
   }
 };
 
-// adding a blog
-export const addBlog = async (req, res) => {
+// updating a blog
+export const editBlog = async (req, res) => {
   try {
-    const newBlog = await  Blog.create(req.body)
+    const blogId = req.query.id;
 
-    return res.status(201).json({
+    const article = await Blog.findOneAndUpdate({ _id: blogId }, req.body, {
+      new: true,
+    });
+
+    res.json({
       success: true,
-      message: newBlog
-    })
+      data: article,
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      message: "Error try again!",
+    });
+  }
+};
+
+// delete a blog
+export const removeBlog = async (req, res) => {
+  try {
+    const blogId = req.query.id;
+
+    const deletedBlog = await Blog.deleteOne({ _id: blogId });
+
+    res.json({
+      success: true,
+      deletedCount: deletedBlog.deletedCount,
+    });
   } catch (error) {
     console.log(error);
-    res.Status(500).json({
+    return res.status.json({
       success: false,
-      message: "Failed",
+      message: error,
     });
   }
 };
--- server/src/routes/blogRoute.js
+++ server/src/routes/blogRoute.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
-import { addBlog, getBlog } from "../controllers/blogs.js";
+import {
+  addBlog,
+  editBlog,
+  getBlog,
+  removeBlog,
+  singleBlog,
+} from "../controllers/blogs.js";
 import { userAuthentication } from "../middleware/Auth.js";
 
 const blogRouter = Router();
 
-blogRouter.route("/blogs").get(getBlog).post(userAuthentication ,addBlog);
+blogRouter
+  .route("/blogs")
+  .get(getBlog)
+  .post(singleBlog)
+  .post(userAuthentication, addBlog)
+  .patch(userAuthentication, editBlog)
+  .delete(userAuthentication, removeBlog);
 
 export { blogRouter };
