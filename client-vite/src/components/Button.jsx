9f5c1dc620d0fe8c8c5141043a35c3639bbc7b95
Ariel-Joe <arieljoe00@gmail.com>
2025-01-14 21:23:27 +0300

    button-comp

---
+++ client-vite/src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Button = (props) => {
+  console.log(props);
+
+  return (
+    <>
+      <button
+        style={{
+          backgroundColor: `${props.bgColor ? props.bgColor : "blue"}`,
+          color: "white",
+          padding: "10px 15px",
+          margin: "10px",
+        }}
+      >
+        {props.children}
+      </button>
+    </>
+  );
+};
+
+export { Button };
