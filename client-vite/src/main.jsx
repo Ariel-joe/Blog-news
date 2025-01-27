320446c658b8042139d7fcb324abe0487beeb384
Mark Muthii <mark.muthii@strathmore.edu>
2025-01-27 17:15:13 +0300

    Configure routes

--- client-vite/src/main.jsx
+++ client-vite/src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import "./index.css";
+import { App } from "./App.jsx";
+import { AboutPage } from "./pages/AboutPage.jsx";
+import { HomePage } from "./pages/HomePage.jsx";
+import { NewsPage } from "./pages/NewsPage.jsx";
+import { ContactPage } from "./pages/ContactPage.jsx";
+import { LoginPage } from "./pages/auth/LoginPage.jsx";
+import { SignupPage } from "./pages/auth/SignupPage.jsx";
+import AuthLayout from "./pages/auth/AuthLayout.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App></App>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
