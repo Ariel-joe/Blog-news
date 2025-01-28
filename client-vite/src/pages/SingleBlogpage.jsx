cd6694c14a4f3c1fbb380768ac2d9a02638dd34a
Ariel-Joe <arieljoe00@gmail.com>
2025-01-28 13:12:32 +0300

    useparams hook

--- client-vite/src/pages/SingleBlogpage.jsx
+++ client-vite/src/pages/SingleBlogpage.jsx
@@ -9,7 +9,7 @@ const SingleBlogpage = () => {
     const fetchArticle = async () => {
       try {
         const response = await fetch(
-          `http://localhost:8080/api/blogs/single/?id=${id}`
+          `http://localhost:8080/api/blogs/single/${id}`
         );
 
         const result = await response.json();
@@ -24,12 +24,16 @@ const SingleBlogpage = () => {
   }, [id]);
 
   return (
-    <div>
-      <h1>{article.title}</h1>
-      <img src={article.image} alt="" />
+    <div className="my-3 flex flex-col justify-center items-center">
+      <div className="flex justify-center space-x-9 items-center">
+        <div>
+          <h1 className="text-4xl font-bold mb-3 underline">{article.title}</h1>
+          <h3 className="text-2xl font-semibold mb-3">{article.description}</h3>
+        </div>
+          <img src={article.image} className="w-[40%] mb-3" alt="" />
+      </div>
 
-      <h3>{article.description}</h3>
-      <p>{article.content}</p>
+      <p className="w-[50%] text-justify">{article.content}</p>
     </div>
   );
 };
