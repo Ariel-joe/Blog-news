b22c04178c3e035d2f1e16df3ff3be2542907bdb
Ariel-Joe <arieljoe00@gmail.com>
2025-01-28 13:47:35 +0300

    effects add on

--- client-vite/src/pages/SingleBlogpage.jsx
+++ client-vite/src/pages/SingleBlogpage.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router";
 const SingleBlogpage = () => {
   const [article, setArticle] = useState([]);
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchArticle = async () => {
@@ -14,27 +15,39 @@ const SingleBlogpage = () => {
 
         const result = await response.json();
 
-        console.log(result);
-
         setArticle(result.data);
-      } catch (error) {}
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false)
+      }
     };
 
     fetchArticle();
   }, [id]);
 
   return (
-    <div className="my-3 flex flex-col justify-center items-center">
-      <div className="flex justify-center space-x-9 items-center">
-        <div>
-          <h1 className="text-4xl font-bold mb-3 underline">{article.title}</h1>
-          <h3 className="text-2xl font-semibold mb-3">{article.description}</h3>
-        </div>
-          <img src={article.image} className="w-[40%] mb-3" alt="" />
-      </div>
+    <>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <div className="my-3 flex flex-col justify-center items-center">
+          <div className="flex justify-center space-x-9 items-center">
+            <div>
+              <h1 className="text-4xl font-bold mb-3 underline">
+                {article.title}
+              </h1>
+              <h3 className="text-2xl font-semibold mb-3">
+                {article.description}
+              </h3>
+            </div>
+            <img src={article.image} className="w-[40%] mb-3" alt="" />
+          </div>
 
-      <p className="w-[50%] text-justify">{article.content}</p>
-    </div>
+          <p className="w-[50%] text-justify">{article.content}</p>
+        </div>
+      )}
+    </>
   );
 };
 
