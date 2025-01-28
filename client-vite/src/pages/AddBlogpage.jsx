182bc34ae8c4ec4cb91eb60ddfb846450432dbd2
Ariel-Joe <arieljoe00@gmail.com>
2025-01-28 10:12:45 +0300

    create method implementation

--- client-vite/src/pages/AddBlogpage.jsx
+++ client-vite/src/pages/AddBlogpage.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const AddBlogpage = () => {
   return (
-    <div>
-      this is where you add the blogs from.
-    </div>
-  )
-}
+    <>
+      <form className="w-[90%] sm:w-2/3 max-w-[650px] mx-auto">
+        <h1 className="mb-8">Add blog/Article</h1>
+        <input
+          type="text"
+          placeholder="title goes here..."
+          className="p-2 border mb-4 border-black w-full"
+        />
+        <input
+          type="text"
+          placeholder="description goes here..."
+          className="p-2 border mb-4 border-black w-full"
+        />
+        <input
+          type="link"
+          placeholder="image link goes here..."
+          className="p-2 border mb-4 border-black w-full"
+        />
 
-export  {AddBlogpage }
+        <textarea
+          type="text"
+          rows={7}
+          placeholder="content goes here..."
+          className="p-2 border mb-4 border-black w-full"
+        />
+
+        <button className="bg-black text-white w-full">Add blog</button>
+      </form>
+    </>
+  );
+};
+
+export { AddBlogpage };
