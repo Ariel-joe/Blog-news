4f0f53108c6f7bae334e3e1e5f0093e3a6c9245c
Ariel-Joe <arieljoe00@gmail.com>
2025-01-30 14:12:49 +0300

    updating the login logic

--- client-vite/src/pages/Auth/Login.jsx
+++ client-vite/src/pages/Auth/Login.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import { GoArrowRight } from "react-icons/go";
 import { Link } from "react-router";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submithandler = async (e) => {
+    e.preventDefault()
+
+    try {
+      const formData = {
+        username, password
+      }
+
+      const response = await fetch("")
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <>
       <h1 className="text-3xl font-semibold text-center mb-14">Welcome Back</h1>
-      <form className="flex flex-col gap-6">
+      <form onSubmit={submithandler} className="flex flex-col gap-6">
         <input
           type="text"
           className="p-2 border"
           placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
           className="p-2 border"
           placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button type="submit" className="bg-black text-white p-2">
