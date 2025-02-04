cbfb57e96dd6029071e991edf2099690eb53b688
Ariel-Joe <arieljoe00@gmail.com>
2025-02-04 12:45:03 +0300

    managing global user state

--- client-vite/src/components/Header.jsx
+++ client-vite/src/components/Header.jsx
@@ -1,7 +1,19 @@
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useUserStore } from "../store/user-store";
+import { toast } from "sonner";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const { logout, isLoggedIn } = useUserStore();
+
+  const handleLogout = () => {
+    logout();
+
+    toast.success("logged out successfully");
+
+    navigate("/login");
+  };
   return (
     <header className="w-1/5 h-full overflow-hidden border-r">
       <nav>
@@ -40,16 +52,35 @@ const Header = () => {
           <hr />
 
           <div className="space-y-6">
-            <li className="border px-2 py-2">
-              <Link to={"/login"} className="flex items-center gap-4 text-lg">
-                Login
-              </Link>
-            </li>
-            <li className="border px-2 py-2">
-              <Link to={"/signup"} className="flex items-center gap-4 text-lg">
-                Signup
-              </Link>
-            </li>
+            {isLoggedIn ? (
+              <li
+                onClick={handleLogout}
+                className="border px-2 py-2 bg-red-500 text-white cursor-pointer"
+              >
+                <button className="flex items-center gap-4 text-lg">
+                  Logout
+                </button>
+              </li>
+            ) : (
+              <>
+                <li className="border px-2 py-2">
+                  <Link
+                    to={"/login"}
+                    className="flex items-center gap-4 text-lg"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li className="border px-2 py-2">
+                  <Link
+                    to={"/signup"}
+                    className="flex items-center gap-4 text-lg"
+                  >
+                    Signup
+                  </Link>
+                </li>
+              </>
+            )}
           </div>
         </ul>
       </nav>
