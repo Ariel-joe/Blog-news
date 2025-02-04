aa11eb3bad9719ede972bc79203273de025364af
Ariel-Joe <arieljoe00@gmail.com>
2025-02-04 12:16:16 +0300

    i zustand for global state management

---
+++ client-vite/src/store/user-store.js
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+const userStore = create((set) => ({
+    user: null,
+    isLoggedIn: false,
+    login: async (userData) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}))
