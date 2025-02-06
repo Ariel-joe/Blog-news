b522dd08db07d84ee788170504eaf8912c15edeb
Mark Muthii <mark.muthii@strathmore.edu>
2025-02-06 18:11:19 +0300

    Update client

--- client-vite/src/GlobalLayout.jsx
+++ client-vite/src/GlobalLayout.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 import { Toaster } from "sonner";
 
 const GlobalLayout = () => {
+  useEffect(() => {
+    console.log("App rendered");
+  }, []);
+
   return (
     <>
       <Toaster position="bottom-center" richColors />
--- client-vite/src/components/TopBar.jsx
+++ client-vite/src/components/TopBar.jsx
@@ -2,20 +2,27 @@ import { IoIosNotificationsOutline } from "react-icons/io";
 import { CiUser } from "react-icons/ci";
 import { SearchArticles } from "./SearchArticles";
 import { Link } from "react-router";
+import { useUserStore } from "../store/user-store";
 
 const TopBar = () => {
+  const { isLoggedIn } = useUserStore();
+
   return (
     <div className="topbar flex justify-between px-4 py-4 items-center border-b fixed w-4/5 bg-white">
       <SearchArticles />
       <div className="icons flex gap-4">
-        <span className="notifications border rounded-full p-2 cursor-pointer hover:shadow-lg duration-150">
-          <IoIosNotificationsOutline size={25} />
-        </span>
-        <span className="profile border rounded-full p-2 cursor-pointer hover:shadow-lg duration-150">
-          <Link to={"/profile"}>
-            <CiUser size={25} />
-          </Link>
-        </span>
+        {isLoggedIn && (
+          <>
+            <span className="notifications border rounded-full p-2 cursor-pointer hover:shadow-lg duration-150">
+              <IoIosNotificationsOutline size={25} />
+            </span>
+            <span className="profile border rounded-full p-2 cursor-pointer hover:shadow-lg duration-150">
+              <Link to={"/profile"}>
+                <CiUser size={25} />
+              </Link>
+            </span>
+          </>
+        )}
       </div>
     </div>
   );
