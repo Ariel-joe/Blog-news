21e6c752a847241093ed4bd4238224a5d590722f
Ariel-Joe <arieljoe00@gmail.com>
2025-02-14 16:25:55 +0300

    authorisation

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,3 +1,18 @@
-export const getUsers = (req, res) => {
-  
+import { StatusCodes } from "http-status-codes";
+import { User } from "../database/models/user.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      success: false,
+      message: " you are not authorised",
+    });
+  }
 };
